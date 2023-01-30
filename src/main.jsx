16d6bd6b730ee4966c1d1ef67310cc726f1c3bfa
parkerray <parkerthompson@hey.com>
2023-01-30 16:41:44 -0600

    changed to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import About from './About'
 import Root from './routes/root'
 import ErrorPage from './error-page'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Root />,
