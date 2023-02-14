ebbdb52c25ec34a914ec05f300259efed9623c4b
parkerray <parkerthompson@hey.com>
2023-02-14 09:15:21 -0600

    Fixed API issue

--- src/Top.jsx
+++ src/Top.jsx
@@ -7,22 +7,22 @@ export default function Top() {
   const [isLoading, setIsLoading] = useState(true);
 
 
-  // const fetchData = async () => {
-  //   await fetch('https://main.api314.com/api:public:v1/top-max-holders')
-  //     .then(response => response.json())
-  //     .then(data => setRecords(data));
-  //   setIsLoading(false);
-  // }
+  const fetchData = async () => {
+    await fetch('https://main.api314.com/api:public:v1/indexed-top-holders')
+      .then(response => response.json())
+      .then(data => setRecords(data));
+    setIsLoading(false);
+  }
 
-  // useEffect(() => {
-  //   fetchData();
-  // }, [])
+  useEffect(() => {
+    fetchData();
+  }, [])
 
 
   return (
     <>
       <div className='section-full'>
-        {/* {isLoading ? <p>Loading...</p> :
+        {isLoading ? <p>Loading...</p> :
         <div className='list-wrapper'>
           <div className='list-header'>
             <p>UINTS SUM</p>
@@ -32,11 +32,10 @@ export default function Top() {
             <Holder
               key={record.owner}
               address={record.owner}
-              number={record.max_value}
+              number={record.number}
             />
-          ))} */}
-        {/* </div>} */}
-        <p>Temporarily unavailable</p>
+          ))}
+         </div>}
       </div>
     </>
   )
